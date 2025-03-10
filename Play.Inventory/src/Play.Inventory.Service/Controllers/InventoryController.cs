3a25cb76d24e92f03d11151703968c0daa8371ce
dinhdkhoa <kevin_mccain1718@yahoo.com>
2025-03-10 13:41:01 +0700

    feat: Inventory Crud

---
+++ Play.Inventory/src/Play.Inventory.Service/Controllers/InventoryController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using Play.Common;
+using Play.Inventory.Service.Dtos;
+using Play.Inventory.Service.Entities;
+
+namespace Play.Inventory.Service.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class InventoryController : ControllerBase
+    {
+        private readonly IRepository<InventoryItem> repo;
+        public InventoryController(IRepository<InventoryItem> _repo)
+        {
+            repo = _repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<InventoryItemDto>>> GetAsync(Guid userId)
+        {
+            if (userId == Guid.Empty)
+            {
+                return BadRequest();
+            }
+
+            var inventoryItemEntities = await repo.GetListAsync(item => item.UserId == userId);
+
+            var inventoryItemDtos = inventoryItemEntities.Select(inventoryItem => inventoryItem.AsDto());
+
+            return Ok(inventoryItemDtos);
+        }
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<InventoryItemDto>>> Create(GrantItemsDto req)
+        {
+            if (req.UserId == Guid.Empty)
+            {
+                return BadRequest();
+            }
+
+            var currentItem = await repo.GetAsync(item => item.UserId == req.UserId && item.CatalogItemId == req.CatalogItemId);
+
+            if(currentItem == null){
+                var newItem = new InventoryItem{
+                    AcquiredDate = DateTimeOffset.Now,
+                    Quantity = req.Quantity,
+                    UserId = req.UserId,
+                    CatalogItemId = req.CatalogItemId
+                };
+                await repo.CreateAsync(newItem);
+            }
+            else {
+                currentItem.Quantity += req.Quantity;
+                await repo.UpdateAsync(currentItem);
+            }
+
+            return Ok();
+        }
+    }
+}
--- Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Play.Common.MongoDB;
+using Play.Inventory.Service.Entities;
 
 namespace Play.Inventory.Service
 {
@@ -27,6 +22,7 @@ namespace Play.Inventory.Service
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddMongo().AddMongoRepository<InventoryItem>("inventoryitems");
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
