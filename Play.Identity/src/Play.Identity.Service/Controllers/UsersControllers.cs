6161fb094ecbadead3a93e695dc8bb578fbeae5d
dinhdkhoa <kevin_mccain1718@yahoo.com>
2025-03-31 17:40:13 +0700

    feat: UsersControllers

---
+++ Play.Identity/src/Play.Identity.Service/Controllers/UsersControllers.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Play.Identity.Service.Dtos;
+using Play.Identity.Service.Entities;
+
+namespace Play.Identity.Service.Controllers
+{
+    [ApiController]
+    [Route("users")]
+    public class UsersControllers : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UsersControllers(UserManager<ApplicationUser> _userManager)
+        {
+            userManager = _userManager;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<UserDto>> Get() {
+            var users = userManager.Users.ToList().Select(user => user.AsDto());
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDto>> GetByIdAsync(Guid id)
+        {
+            var user = await userManager.FindByIdAsync(id.ToString());
+            if(user == null) return NotFound();
+            return user.AsDto();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UserDto>> UpdateAsync(Guid id, UpdateUserDto req)
+        {
+            var user = await userManager.FindByIdAsync(id.ToString());
+            if (user == null) return NotFound();
+            user.Email = req.Email;
+            user.Gil = req.Gil;
+            await userManager.UpdateAsync(user);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UserDto>> DeleteAsync(Guid id)
+        {
+            var user = await userManager.FindByIdAsync(id.ToString());
+            if (user == null) return NotFound();
+            await userManager.DeleteAsync(user);
+            return NoContent();
+        }
+    }
+}
