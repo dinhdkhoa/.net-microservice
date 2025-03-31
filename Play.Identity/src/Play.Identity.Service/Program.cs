b0bc0710448b19dd32dfec3fc51a18a7fc2a43b6
dinhdkhoa <kevin_mccain1718@yahoo.com>
2025-03-31 15:39:08 +0700

    feat: UseStaticFiles To Serve Razor Pages

--- Play.Identity/src/Play.Identity.Service/Program.cs
+++ Play.Identity/src/Play.Identity.Service/Program.cs
@@ -5,6 +5,7 @@ using Play.Identity.Service.Entities;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Bson;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,13 +14,16 @@ BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
 
-builder.Services.AddIdentityCore<ApplicationUser>()
-    .AddRoles<ApplicationRole>()
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
     .AddMongoDbStores<ApplicationUser, ApplicationRole, Guid>(mongo =>
     {
         mongo.ConnectionString =  $"{mongoDbSettings.ConnectionString}/{serviceSettings.ServiceName}";
-    }); 
+    }).AddDefaultTokenProviders()
+    .AddDefaultUI();
+builder.Services.AddScoped<SignInManager<ApplicationUser>>(); // 👈 Add this line
+
 builder.Services.AddControllers();
+builder.Services.AddRazorPages();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -34,9 +38,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapRazorPages();
 
 app.Run();
