7729ad7a0d901cf9d7040602a726fc7450e895fc
dinhdkhoa <kevin_mccain1718@yahoo.com>
2025-04-19 16:54:56 +0700

    feat: Initializing The State Machine

--- Play.Trading/src/Play.Trading.Service/StateMachines/PurchaseStateMachine.cs
+++ Play.Trading/src/Play.Trading.Service/StateMachines/PurchaseStateMachine.cs
@@ -1,3 +1,4 @@
+using System;
 using Automatonymous;
 
 namespace Play.Trading.Service.StateMachines
@@ -8,10 +9,33 @@ namespace Play.Trading.Service.StateMachines
         public State ItemsGranted {get; }
         public State Completed {get; }
         public State Faulted {get; }
-        
+        public Event<PurchaseRequested> PurchaseRequested { get; }
         public PurchaseStateMachine()
         {
             InstanceState(state => state.CurrentState);
+            ConfigureEvents();
+            ConfigureInitState();
+        }
+
+        private void ConfigureEvents()
+        {
+            Event(() => PurchaseRequested);
+        }
+
+        private void ConfigureInitState()
+        {
+            Initially(
+                When(PurchaseRequested)
+                .Then(context => 
+                {
+                    context.Instance.UserId = context.Data.UserId;
+                    context.Instance.ItemId = context.Data.ItemId;
+                    context.Instance.Quantity = context.Data.Quantity;
+                    context.Instance.ReceivedAt = DateTimeOffset.UtcNow;
+                    context.Instance.UpdatedAt = context.Instance.ReceivedAt;
+                })
+                .TransitionTo(Accepted)
+            );
         }
     }
 }
