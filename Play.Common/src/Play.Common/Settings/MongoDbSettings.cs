343e789016745162176239943e76b9a2a2370b8a
dinhdkhoa <kevin_mccain1718@yahoo.com>
2025-05-16 14:48:32 +0700

    feat: Update Play.Common MongoDbSettings To Use Production URL String

--- Play.Common/src/Play.Common/Settings/MongoDbSettings.cs
+++ Play.Common/src/Play.Common/Settings/MongoDbSettings.cs
@@ -7,9 +7,22 @@ namespace Play.Common.Settings
 {
     public class MongoDbSettings
     {
-        public string Host {get ; init;}
-        public int Port {get ; init;}
+        private string connectionString;
 
-        public string ConnectionString => $"mongodb://{Host}:{Port}";
+        public string Host { get; init; }
+        public int Port { get; init; }
+
+        // public string ConnectionString => $"mongodb://{Host}:{Port}";
+        public string ConnectionString
+        {
+            get
+            {
+                return string.IsNullOrWhiteSpace(connectionString)
+                        ? $"mongodb://{Host}:{Port}" : connectionString;
+            }
+            init { connectionString = value; }
+        }
+
     }
 }
+
